497b51960b2a8fab84de0bc26bf226657ecec7bb
Faisal Chowdhury <abdullahalfaisalchowdhury786@gmail.com>
2021-02-02 16:49:17 +0600

    Interview q about null/undefined

---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;(kno value declare na korle)
+
+function add(num1, num2) {
+    console.log(num1+num2);
+    return
+} (ki return korbe ta na bole dile or kno kichu return na korle)
+
+function add(num1, num2) {
+    console.log(num1,num2);
+} (kno parameter pass na korle)
+
+const profile={ name:"Shoeib", phone: 01819362702};
+console.log(profile.adress);  (kno property nai,but if you want to access)
+
+let fun= undefined;
+console.log(fun);
+
+const ages=[2,5,6];
+console.log(ages[11]);
